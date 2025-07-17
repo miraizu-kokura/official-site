8da7f2a1e7df776fcf8a0d4887c7dfcb0e0829a7
Sakiko Yamane <yamane.sakiko.0120@gmail.com>
2025-07-18 00:41:57 +0900

    メインビジュアルとコンセプトセクションの作成

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,3 +5,12 @@ menuBtn.addEventListener('click', () => {
  menu.classList.toggle('is-active');
  menuBtn.classList.toggle('is-open');
 });
+
+window.addEventListener('scroll', function () {
+ const header = document.querySelector('.header');
+ if (window.scrollY > 100) {
+  header.classList.add('fixed');
+ } else {
+  header.classList.remove('fixed');
+ }
+});
